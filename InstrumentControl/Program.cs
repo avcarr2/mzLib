e76464b6d3779a041cf2d76b12a9cfefc005d390
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2022-03-02 15:05:04 -0600

    changed datareceiver to abstract class
    i changed DataReceiver to abstract class

--- InstrumentControl/Program.cs
+++ InstrumentControl/Program.cs
@@ -10,17 +10,30 @@ namespace InstrumentControl
 	{
 		public static void Main(string[] args)
 		{
-			FusionLumosAPI api = InitializeConnection();
-			DataReceiver dataReceiver = new(); 
 
+			FusionLumosAPI api = InitializeConnection();
+			RealTimeSampleProcessingExample dataReceiver = new(); 
+
+			// main loop 
 			while (api.InstAccessContainer.ServiceConnected)
 			{
+				// MsScanArrived is an event.
+				// The += means "subscribes to".
+				// So the data.Receiver.MSScanContainer_MsScanArrived method is
+				// subscribed to the MsScanArrived event and will be called when the event is raised.
+
+				/* Technically, multiple methods can subscribe to the same event, 
+				 * however, it is likely doing so would require multithreading. 
+				 * You would need to have multiple objects working with a single IMsScan object, 
+				 * and I'm not sure how you would pass a single IMsScan to multiple threads yet
+				 */
 				api.MSScanContainer.MsScanArrived += dataReceiver.MSScanContainer_MsScanArrived; 
 				
 			}
 			
 		}
-		public static FusionLumosAPI InitializeConnection()
+		// Instantiates the FusionLumosAPI and starts both online access and instrument access
+		private static FusionLumosAPI InitializeConnection()
 		{
 			FusionLumosAPI api = new();
 			api.StartOnlineAccess(); // call to start online connection. Need to add event and error handling. 
---
+++ InstrumentControl/RealTimeSampleProcessingExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermo.Interfaces.FusionAccess_V1;
+using Thermo.Interfaces.FusionAccess_V1.MsScanContainer;
+using Thermo.Interfaces.InstrumentAccess_V1.MsScanContainer;
+using Thermo.Interfaces.InstrumentAccess_V1.Control.Scans;
+using Thermo.Interfaces.SpectrumFormat_V1;
+using Thermo.Interfaces.FusionAccess_V1.Control.Scans;
+using System.Threading;
+using MassSpectrometry;
+
+namespace InstrumentControl
+{
+    public class RealTimeSampleProcessingExample : DataReceiver
+    {
+		/*
+		 Method classes inheriting from DataReceiver must implement a ScanProcessingQueue
+		(Queue<MsDataScan>) and the MSScanContainer_MsScanArrived method.
+
+		To implement the property and method, you must use the override keyword.
+
+		After the abstract method overrides, implement the "engine" method, where 
+
+		 */
+
+		// private field used to initialize the scan processing queue
+		private Queue<MsDataScan> _scanProcessingQueue = new Queue<MsDataScan>(); 
+		// ScanProcessingQueue is the public property; it uses the getter to set the private
+		// field to the public property. This is the correct way to initialize a publicly
+		// available object like Queues, Lists, and Dicts as properties.
+		public override Queue<MsDataScan> ScanProcessingQueue { get { return _scanProcessingQueue; }}
+
+		public override void MSScanContainer_MsScanArrived(object sender, MsScanEventArgs e)
+		{
+			// put scan in queue
+			// Keeping IMScan open will hog the memory resources, so you need to get the  
+
+			using (IMsScan scan = e.GetScan())
+			{
+				// need to quickly convert to something else
+				// ScanProcessingQueue.Enqueue(scan);
+				ScanProcessingQueue.Enqueue(new MsDataScan(scan));
+			}
+			// Perform data processing below here. For clarity, use the minimum
+			// number of method calls possible, preferablly a single method,
+			// for example, ProteoformIdentifcationEngine(ProteoformIDParameters params);
+			// Engine should return either void or an ICustomScan object, but I need to implement the
+			// code to facilitate the building and sending of the ICustomScan. 
+
+			ProteoformProcessingEngine();
+
+			// TODO: Implement ICustomScan wrapper to facilitate custom scan sending.
+            // May need to wait till Chris Rose has figured everything with Thermo out. 
+		}
+
+		// other methods used in processing go below this comment:
+		public void ProteoformProcessingEngine()
+        {
+			// engine should try to return void, bool, or an ICustomScan object (or both bool and ICustomScan) 
+        }
+	}
+}
