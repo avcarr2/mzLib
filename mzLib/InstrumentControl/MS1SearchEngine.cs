43a0bcacfe3712d21237f99fc9a0779a8eab6b21
Nic Bollis <nbollis@comcast.net>
2022-04-05 15:07:27 -0600

    Finished implementing discussed changes
    Changed the PeakScorer to use linq FindAll().
    Added in ability to do simple boolean found = 1, not = 0 search.
    Occurrences mode will report how many database entries match the mass within tolerance range.
    Made Method to load scans from files. Will need to add bruker support at some point.
    Did general cleanup

--- mzLib/InstrumentControl/MS1SearchEngine.cs
+++ mzLib/InstrumentControl/MS1SearchEngine.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using MassSpectrometry;
-using Proteomics.Fragmentation;
-using Proteomics.ProteolyticDigestion;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +21,7 @@ namespace InstrumentControl
         }
 
         // Will process and the scan and returns a scoreTable with a high number representing a likely chance of the fragment existing in the database
-        public void PeakScorer(MsDataScan scan, out List<IsotopicEnvelope> envelopes, out int[] scoreTable)
+        public void FindPeakWithinDatabase(MsDataScan scan, out List<IsotopicEnvelope> envelopes, out int[] scoreTable, string searchType = "occurrences")
         {
             // deconvolue scan (eventually switch to unidec?)
             int minAssumedChargeState = 2;
@@ -31,30 +29,40 @@ namespace InstrumentControl
             int deconvolutionTolerancePpm = 6;
             int intensityRatio = 3;
             envelopes = scan.MassSpectrum.Deconvolute(scan.MassSpectrum.Range, minAssumedChargeState, maxAssumedChargeState, deconvolutionTolerancePpm, intensityRatio).ToList();
+            scoreTable = new int[envelopes.Count];
+            var envelopesWithMassInDatabase = envelopes.FindAll(FindWithinMass);
 
-            double maxIntensity = 0;
-            if (envelopes.Count() > 0)
+            switch (searchType)
             {
-                maxIntensity = envelopes.Max(p => p.TotalIntensity);
-            }
-            scoreTable = new int[envelopes.Count()];
-
-            // Go through scans and increment the score if there is a protein found in database that matches its monomass w/n tolerance
-            for (int i = 0; i < envelopes.Count(); i++)
-            {
-                if (envelopes[i].TotalIntensity <= maxIntensity / 1000) // May need to adjust this number over time
-                    continue;
-                double mass = envelopes[i].MonoisotopicMass;
-                double lowestMassToSearch = Tolerance.GetMinimumValue(mass);
-                double highestMassToSearch = Tolerance.GetMaximumValue(mass);
-
-                // TOTRY: Can redo this with a for loop from min to max performing a binary search for the mass in question. Can try after functioning and compare times
-                // TOTRY: Instead of using an int, do a simple bool binary -> this will reduce the calcuations needed in the subsequent comparison steps for peak picking but reduces the ability to use other factors in the scoring in the future
-                if (Database.ProteinList.Any(p => p.MonoisotopicMass >= lowestMassToSearch && p.MonoisotopicMass <= highestMassToSearch))
-                {
-                    scoreTable[i] = Database.ProteinList.Count(p => Tolerance.Within(mass, p.MonoisotopicMass));
-                }
+                // score represents the number of masses in the database within tolerance
+                case "occurrences": 
+                    foreach (var match in envelopesWithMassInDatabase)
+                    {
+                        scoreTable[envelopes.IndexOf(match)] = Database.ProteinList.Count(p => Tolerance.Within(match.MonoisotopicMass, p.MonoisotopicMass));
+                    }
+                    break;
+                // score of 1 means the mass was found within the database within tolerance
+                case "boolean":
+                    foreach (var match in envelopesWithMassInDatabase)
+                    {
+                        scoreTable[envelopes.IndexOf(match)] = 1;
+                    }
+                    break;
             }
         }
+
+        // Explicit predicate delegate for finding if the envelope exists within the database by monoisotopic mass 
+        private bool FindWithinMass(IsotopicEnvelope envelope)
+        {
+            if (Database.ProteinList.Any(p => p.MonoisotopicMass >= Tolerance.GetMinimumValue(envelope.MonoisotopicMass) && p.MonoisotopicMass <= Tolerance.GetMaximumValue(envelope.MonoisotopicMass)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
