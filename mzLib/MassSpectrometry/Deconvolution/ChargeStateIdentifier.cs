cbf256e53414c5ba245814032596ca3cba473db6
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2023-04-18 10:57:13 -0500

    Initial charge state-based deconvoluter. Not automated. Still bare bones

---
+++ mzLib/MassSpectrometry/Deconvolution/ChargeStateIdentifier.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Easy.Common.Interfaces;
+
+namespace MassSpectrometry;
+
+public class ChargeStateIdentifier
+{
+    public static MzSpectrum DownsampleData(MzSpectrum spectrum, int downsampleInt)
+    {
+        return new MzSpectrum(
+            spectrum.XArray.Where((_, index) => index % downsampleInt == 0)
+                .Select(i => i)
+                .ToArray(),
+            spectrum.YArray.Where((_, index) => index % downsampleInt == 0)
+                .Select(i => i)
+                .ToArray(),
+            true
+        ); 
+    }
+
+    public static double[] MovingAverage(MzSpectrum spectrum, int window)
+    {
+        double[] buffer = new double[window];
+        double[] output = new double[spectrum.YArray.Length]; 
+        int currentIndex = 0;
+
+        for (int i = 0; i < spectrum.YArray.Length; i++)
+        {
+            buffer[currentIndex] = spectrum.YArray[i] / window;
+            double ma = 0d;
+            for (int j = 0; j < window; j++)
+            {
+                ma += buffer[j]; 
+            }
+
+            output[i] = ma;
+            currentIndex = (currentIndex + 1) % window; 
+        }
+
+        return output; 
+    }
+    public static IOrderedEnumerable<Tuple<int,double>> IdentifyMaxima(MzSpectrum spectrum, int movingAverageWindow, int localMaxWindowSize)
+    {
+        var downsampledSpectrum = DownsampleData(spectrum, 5);
+        var reducedResolutionPeaks = MovingAverage(downsampledSpectrum, movingAverageWindow);
+        return FindLocalMaxima(reducedResolutionPeaks, localMaxWindowSize).OrderByDescending(i => i.Item2);
+    }
+
+    public static IEnumerable<ChargeStateLadder> CreateChargeStateLadders(List<Tuple<int,double>> indexIntensityTupleList, double[] mzValueArray, int minCharge, int maxCharge, 
+        double minMzValAllowed, double maxMzValAllowed, double adductMass = 1.007276)
+    {
+        // find the maximum intensity peak and use the index to get the mz values from the mzValueArray
+        // (should be the first because its ordered by descending)
+        int indexOfMostIntenseY = indexIntensityTupleList.First().Item1;
+        double mzVal = mzValueArray[indexOfMostIntenseY];
+
+        for (int i = minCharge; i <= maxCharge; i++)
+        {
+            double tempMass = (mzVal - adductMass) * (double)i; 
+            List<(int charge, double mz)> tempLadder = new();
+            for (int j = maxCharge; j > 0; j--)
+            {
+                tempLadder.Add((j,tempMass / (double)j + adductMass));
+            }
+            // clean up the ladder 
+            tempLadder = tempLadder.Where(k => k.mz <= maxMzValAllowed && k.mz >= minMzValAllowed).ToList(); 
+            yield return new ChargeStateLadder(tempMass, tempLadder.Select(k => k.charge).ToArray(), 
+                i, tempLadder.Select(k => k.mz).ToArray());
+        }
+    }
+
+    public static List<(double mz,double intensity)> CreateMzValueArray(MzSpectrum spectrum, List<Tuple<int, double>> indexIntensityTupleList)
+    {
+        var orderedByIndex = indexIntensityTupleList.OrderBy(i => i.Item1);
+        List<(double, double)> outputList = new List<(double, double)>();  
+        foreach (var index in orderedByIndex)
+        {
+            outputList.Add((spectrum.XArray[index.Item1], index.Item2));
+        }
+        return outputList;
+    }
+
+    public static List<(double,double)> MatchChargeStateLadders(MzSpectrum spectrum, List<ChargeStateLadder> chargeStateLadders,
+        List<Tuple<int, double>> indexIntensityTupleList, double ppmMatchTolerance, out List<List<int>> ladderToIndicesMap)
+    {
+        var mzIntensityPairs = CreateMzValueArray(spectrum, indexIntensityTupleList).OrderBy(i => i.mz).ToList(); 
+        ; 
+        ladderToIndicesMap = new List<List<int>>(); 
+        foreach (var ladder in chargeStateLadders)
+        {
+            List<int> outputArray = new();
+            for (int i = 0; i < ladder.MzVals.Length; i++)
+            {
+                for (int j = 0; j < mzIntensityPairs.Count; j++)
+                {
+                    double tolerance = ppmMatchTolerance / 1e6 * ladder.MzVals[i];
+                    double upperTol = ladder.MzVals[i] + tolerance;
+                    double lowerTol = ladder.MzVals[i] - tolerance;
+                    if (mzIntensityPairs[j].mz >= lowerTol && mzIntensityPairs[j].mz <= upperTol)
+                    {
+                        outputArray.Add(j);
+                    }
+                }
+            }
+            
+            ladderToIndicesMap.Add(outputArray);
+        }
+
+        return mzIntensityPairs; 
+    }
+
+    public static IEnumerable<ChargeStateLadderMatch> TransformToChargeStateLadderMatch(List<List<int>> ladderToIndicesMap, List<(double,double)> mzIntensityList, List<ChargeStateLadder> ladders)
+    {
+        for (var index = 0; index < ladderToIndicesMap.Count; index++)
+        {
+            var indicesSet = ladderToIndicesMap[index];
+            var ladder = ladders[index];
+
+            List<double> listMzVals = new();
+            List<double> listIntVals = new();
+            foreach (var indices in indicesSet)
+            {
+                listMzVals.Add(mzIntensityList[indices].Item1);
+                listIntVals.Add(mzIntensityList[indices].Item2);
+            }
+
+            List<double> chargesOfMatchingPeaks = listMzVals.Select(i => (ladder.Mass - 1.00727) / i).ToList();
+
+            ChargeStateLadderMatch ladderMatch = new()
+            {
+                MatchingMzPeaks = listMzVals,
+                IntensitiesOfMatchingPeaks = listIntVals,
+                TheoreticalLadder = ladder, 
+                ChargesOfMatchingPeaks = chargesOfMatchingPeaks
+            }; 
+            yield return ladderMatch;
+        }
+    }
+
+    public static void RemoveIdentifiedMzVals()
+    {
+        throw new NotImplementedException();
+    }
+
+
+// link to where I got this code in stackOverFlow: https://stackoverflow.com/a/18950824
+    public static IEnumerable<Tuple<int, double>> FindLocalMaxima(IEnumerable<double> array, int windowSize)
+    {
+        // Round up to nearest odd value
+        windowSize = windowSize - windowSize % 2 + 1;
+        int halfWindow = windowSize / 2;
+
+        int index = 0;
+        var before = new Queue<double>(Enumerable.Repeat(double.NegativeInfinity, halfWindow));
+        var after = new Queue<double>(array.Take(halfWindow + 1));
+
+        foreach (double d in array.Skip(halfWindow + 1).Concat(Enumerable.Repeat(double.NegativeInfinity, halfWindow + 1)))
+        {
+            double curVal = after.Dequeue();
+            if (before.All(x => curVal > x) && after.All(x => curVal >= x))
+            {
+                yield return Tuple.Create(index, curVal);
+            }
+
+            before.Dequeue();
+            before.Enqueue(curVal);
+            after.Enqueue(d);
+            index++;
+        }
+    }
+
+    public static IEnumerable<Tuple<int, double>> ConvertToIndexedTuple(IEnumerable<double> array)
+    {
+        int index = 0; 
+        foreach (double d in array)
+        {
+            yield return Tuple.Create(index, d);
+            index++; 
+        }
+    }
+}
+
+public record struct ChargeStateLadder
+{
+    public double Mass;
+    public int[] ChargeStatesContained;
+    public int ChargeStateOfMostIntenseMass;
+    public double[] MzVals;
+
+    public ChargeStateLadder(double mass, int[] chargeStatesContained,
+        int chargeStateOfMostIntenseMass, double[] mzVals)
+    {
+        Mass = mass; 
+        ChargeStatesContained = chargeStatesContained;
+        ChargeStateOfMostIntenseMass = chargeStateOfMostIntenseMass;
+        MzVals = mzVals;
+    }
+}
+
+public class ChargeStateLadderMatch
+{
+    public ChargeStateLadder TheoreticalLadder { get; set; }
+    public List<double> MatchingMzPeaks { get; set; }
+    public List<double> IntensitiesOfMatchingPeaks { get; set; }
+    public List<double> ChargesOfMatchingPeaks { get; set; }
+}
---
+++ mzLib/Test/TestChargeStateIdentifier.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+using Readers; 
+namespace Test;
+using NUnit.Framework; 
+using MassSpectrometry;
+
+public class TestChargeStateIdentifier
+{
+    [Test]
+    public void TestChargeStateIdentification()
+    {
+        string path = Path.Combine(TestContext.CurrentContext.TestDirectory, "DataFiles",
+            "ExampleChargeStateIdentifierTest.raw");
+
+        var scan = MsDataFileReader.GetDataFile(path).LoadAllStaticData().GetAllScansList().First().MassSpectrum;
+        //scan = ChargeStateIdentifier.DownsampleData(scan, 5); 
+        var localMaxEnum = ChargeStateIdentifier.ConvertToIndexedTuple(scan.YArray);
+        var orderedEnum = localMaxEnum.OrderByDescending(i => i.Item2).ToList(); 
+        var chargeStateLadders = ChargeStateIdentifier.CreateChargeStateLadders(orderedEnum, scan.XArray, 5, 100, 
+            500, 2000).ToList();
+        var matchedLadders = ChargeStateIdentifier.MatchChargeStateLadders(scan, chargeStateLadders, orderedEnum, 5, out var ladderToIndicesMap);
+        var output = ChargeStateIdentifier.TransformToChargeStateLadderMatch(ladderToIndicesMap, matchedLadders, chargeStateLadders.ToList()).ToList();
+
+        int indexer = 0; 
+        // calculate fraction of masses occurring above 0.05 relative intensity
+        // you're going to have junk values that are additive to the harmonics, but not the true matching value.
+        // so I'm multiplying the percent of the theoretical peaks identified in the charge state envelope times the intensity explained to
+        // reduce the intensity explained by the harmonics while preserving that of the original peak. 
+
+        var bestScoringChargeStateLadderMatch = ScoreChargeStateLadderMatches(output, scan);
+        // convert to isotopic envelopes. 
+            // match to averagine? 
+        
+
+
+    }
+
+    public ChargeStateLadderMatch ScoreChargeStateLadderMatches(List<ChargeStateLadderMatch> ladderMatches, MzSpectrum scan)
+    {
+        return ladderMatches.OrderByDescending(i =>
+            ScoreByIntensityExplained(i, scan) * CompareTheoreticalNumberChargeStatesVsActual(i)).First();
+    }
+
+    public void CleanUpChargeStateLadderMatch(ChargeStateLadderMatch match, MzSpectrum spectra)
+    {
+        // examine the end and remove if non-consecutive. 
+        double[] diffs = new double[match.ChargesOfMatchingPeaks.Count - 1];
+        // if diff is greater than 1 => you need to check to see if the other, intermediate charge states exist
+        // criteria for if a charge state 
+        for (int i = 0; i < diffs.Length; i++)
+        {
+            double diff = match.ChargesOfMatchingPeaks[i] - match.ChargesOfMatchingPeaks[i + 1]; 
+
+            double tolerance = Math.Abs(1 - Math.Round(diff));
+            if (tolerance < 0.1)
+            {
+                continue;
+            }else if (tolerance >= 2)
+            {
+                match.ChargesOfMatchingPeaks.RemoveAt(i + 1);
+                match.IntensitiesOfMatchingPeaks.RemoveAt(i + 1);
+                match.MatchingMzPeaks.RemoveAt(i + 1);
+            }
+
+            double chargeState1 = match.ChargesOfMatchingPeaks[i];
+            double chargeState2 = match.ChargesOfMatchingPeaks[i + 1];
+            while (chargeState1 > chargeState2)
+            {
+                // look for the intermediate peaks in the original data. 
+                //Array.BinarySearch()
+                
+                //spectra.XArray
+
+                //chargeState1--; 
+            }
+
+        }
+
+
+        // examine the middle and see if there are any missing charge states. Check the data at an increased tolerance to see if they exist. 
+        
+    }
+
+    public void QualityControlOutputEnvelopes(List<ChargeStateLadderMatch> ladderMatches)
+    {
+
+    }
+
+    public double CompareTheoreticalNumberChargeStatesVsActual(ChargeStateLadderMatch ladderMatch)
+    {
+        return ladderMatch.ChargesOfMatchingPeaks.Count / (double)ladderMatch.TheoreticalLadder.MzVals.Length; 
+    }
+
+    public double ScoreSequentialChargeStates(ChargeStateLadderMatch ladderMatch)
+    {
+        double[] output = new double[ladderMatch.ChargesOfMatchingPeaks.Count - 1]; 
+        for (int i = 0; i < ladderMatch.ChargesOfMatchingPeaks.Count - 1; i++)
+        {
+            output[i] = ladderMatch.ChargesOfMatchingPeaks[i] - ladderMatch.ChargesOfMatchingPeaks[i + 1]; 
+        }
+
+        return 1 / output.Sum() / (double)output.Length; 
+    }
+
+
+    public double ScoreByIntensityExplained(ChargeStateLadderMatch match, MzSpectrum spectrum, double threshold = 0.05)
+    {
+        return match.IntensitiesOfMatchingPeaks.Select(i => i / spectrum.YArray.Max()).Where(i => i >= threshold).Sum();
+    }
+
+    public void CalculateMasses(ChargeStateLadderMatch match)
+    {
+        double[] masses = new double[match.ChargesOfMatchingPeaks.Count];
+        for (int i = 0; i < match.ChargesOfMatchingPeaks.Count; i++)
+        {
+            //match.ChargesOfMatchingPeaks[i]
+        }
+    }
+
+    [Test]
+    public void TestCreateChargeStateLadder()
+    {
+        double mass = 1000;
+        int charge = 1;
+        double adductMass = 1.0078; 
+
+        double exptlMass = (mass + charge * adductMass) / charge;
+        double exptlMass2 = mass / charge + adductMass;
+        Console.WriteLine(exptlMass + "; " + exptlMass2);
+        Console.WriteLine("{0}", (exptlMass - adductMass) * charge);
+    }
+}
